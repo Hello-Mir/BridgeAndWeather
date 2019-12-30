1c3d9da09dca4a18e515c8188ad5b60747fbaeda
atlas <yessss42@gmail.com>
2019-12-30 18:55:22 +0300

    Bridge/stickCoordinator in progress

--- src/main/kotlin/Bridge.kt
+++ src/main/kotlin/Bridge.kt
@@ -1,19 +1,32 @@
 import kotlin.random.Random
 
-class Bridge (val someWidth: Int, val someLength: Int) : Observer {
-    private val width: Int = 0
-    private val lenght: Int = 0
+class Bridge(val width: Int, val length: Int) : Observer {
+    //    private val localWidth: Int = 0
+//    private val lenght: Int = 0
     private var gotThunder: Boolean = false
 
     override fun update(thunderStrike: Boolean) {
         gotThunder = thunderStrike
     }
 
-    fun randomGeneration():Int{
-        return 1+ Random.nextInt(2)
+    fun randomGeneration(): Int {
+        return 1 + Random.nextInt(2)
     }
 
-    fun stickCoordinatsCalculator(someBridge: Bridge){
+    fun stickCoordinatsCalculator(someBridge: Bridge): List<Stick> {
+        val lastStick = Stick(someBridge.length, someBridge.width)
+        val listOfSticks: MutableList<Stick> = mutableListOf(lastStick)
+        var lastX = lastStick.xLocation
+        var lastY = lastStick.yLocation
+
+
+        for (stick in listOfSticks) {
+            if (stick.xLocation > 0 && lastY > 0)
+                listOfSticks.add(Stick(lastX, lastY))
+            lastX -= 1
+            lastY -= 1
+        }
+        return listOfSticks
 /*
 получать экземпляр моста
 составить список опор с конкретными координатами
--- src/main/kotlin/BridgeConstructor.kt
+++ src/main/kotlin/BridgeConstructor.kt
@@ -1,4 +1,8 @@
 fun main(){
     val test = Bridge(2, 3)
-    print(test.randomGeneration())
+//    print(test.randomGeneration())
+    val someSticks = test.stickCoordinatsCalculator(test)
+    println(someSticks.get(1).xLocation)
+
+
 }
